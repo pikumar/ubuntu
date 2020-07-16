eaeef7610719b1c1a12df04e906d2ebc8f8b8b94
Piyush Kumar <piyush@compgeom.com>
2020-07-16 11:37:32 -0400

    adding -y

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def install_signal():
     cmd = """
     curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
     echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
-    sudo apt update && sudo apt install signal-desktop
+    sudo apt update && sudo apt install signal-desktop -y
     """
     if not Path("/etc/apt/sources.list.d/signal-xenial.list").is_file():
         check_call(cmd, shell=True)
